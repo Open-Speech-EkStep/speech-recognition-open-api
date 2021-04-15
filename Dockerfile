b9ab1d5e15431aa6dfb7aa0f4b84230e90f41bd7
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-04-15 13:44:44 +0530

    |Soujyo + niresh | Dockerize the grpc server code

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.8.6
+
+RUN apt-get update && apt-get upgrade -y
+#RUN apt-get install -y wget
+#RUN apt-get install -y curl
+#RUN apk add --update --no-cache \
+#    gcc \
+#    linux-headers \
+#    make \
+#    musl-dev \
+#    python3-dev \
+#    g++
+
+RUN mkdir /opt/speech_recognition_open_api/
+WORKDIR /opt/speech_recognition_open_api/
+
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY . /opt/speech_recognition_open_api
+EXPOSE 50051
+CMD ["python","/opt/speech_recognition_open_api/server.py"]
--- examples/python/speech-recognition/main.py
+++ examples/python/speech-recognition/main.py
@@ -2,9 +2,10 @@ import grpc
 from proto_lib.speech_recognition_open_api_pb2_grpc import SpeechRecognizerStub
 from proto_lib.speech_recognition_open_api_pb2 import RecognitionInput
 
-with grpc.insecure_channel('localhost:50051') as channel:
-    stub = SpeechRecognizerStub(channel)
-    url = "https://teest/s.mp3"
-    response = stub.recognize(RecognitionInput(audio_url=url))
-    print("Successfully requested and response received" if response.result == url else "Not working ! Check again")
-    # print(response.result)
+if __name__ == '__main__':
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = SpeechRecognizerStub(channel)
+        url = "https://teest/s.mp3"
+        response = stub.recognize(RecognitionInput(audio_url=url))
+        print("Successfully requested and response received" if response.result == url else "Not working ! Check again")
+        # print(response.result)
