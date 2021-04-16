c7b655f0f34eb71c295b9dc10f11ee87f7db9cc6
NireshKumarRaj <nireshkumar.r@thoughtworks.com>
2021-04-16 10:09:17 +0000

    |Niresh| Added model and utility support for api

--- speech_recognition_service.py
+++ speech_recognition_service.py
@@ -1,26 +1,24 @@
 from stub import speech_recognition_open_api_pb2_grpc
 from stub.speech_recognition_open_api_pb2 import RecognitionOutput
-
-
-# from model_service import ModelService
+from utilities import download_from_url_to_file,create_wav_file_using_bytes,write_to_file
+from model_service import ModelService
 
 class SpeechRecognizer(speech_recognition_open_api_pb2_grpc.SpeechRecognizerServicer):
 
     def __init__(self):
-        # model_dict_path = "./../wav2vec_vakyansh/model_dict.json"
-        # self.model_service = ModelService(model_dict_path)
-        # print("Model loaded successfully")
-        pass
+        model_dict_path = "./../wav2vec_vakyansh/model_dict.json"
+        self.model_service = ModelService(model_dict_path)
+        print("Loaded successfully")
 
     def recognize(self, request, context):
-        # audio_path = './../wav2vec_vakyansh/xEOIIzS5VySDKU9OAAAB1.wav'
-        # language = 'en-IN'
+        print("Received", request.language, request.file_name)
+        if len(request.audio_url) != 0:
+            audio_path = download_from_url_to_file(request.file_name, request.audio_url)
+        else:
+            audio_path = create_wav_file_using_bytes(request.file_name, request.audio_bytes)
+        response = self.model_service.transcribe(audio_path, request.language)
+        return RecognitionOutput(result=response['transcription'])
         # if len(request.audio_url) != 0:
-        #     response = self.model_service.transcribe(audio_path, language)
-        #     return RecognitionOutput(result=response['transcription'])
+        #     return RecognitionOutput(result=request.audio_url)
         # else:
         #     return RecognitionOutput(result=str(request.audio_bytes))
-        if len(request.audio_url) != 0:
-            return RecognitionOutput(result=request.audio_url)
-        else:
-            return RecognitionOutput(result=str(request.audio_bytes))
