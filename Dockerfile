449339f2f369ac186a18d49649f5dc86947e8d4a
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-04-19 15:15:56 +0530

    |Soujyo + niresh | Add model dict and bootstrap code

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.8.6
 
 RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y sudo
 #RUN apt-get install -y wget
 #RUN apt-get install -y curl
 #RUN apk add --update --no-cache \
@@ -18,5 +19,7 @@ WORKDIR /opt/speech_recognition_open_api/
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . /opt/speech_recognition_open_api
+RUN sudo chown 777 /opt/speech_recognition_open_api/model_bootstrap.sh
 EXPOSE 50051
+CMD ["sh","/opt/speech_recognition_open_api/model_bootstrap.sh"]
 CMD ["python","/opt/speech_recognition_open_api/server.py"]
--- model_bootstrap.sh
+++ model_bootstrap.sh
@@ -1,5 +1,6 @@
-sudo apt-get install liblzma-dev libbz2-dev libzstd-dev libsndfile1-dev libopenblas-dev libfftw3-dev libgflags-dev libgoogle-glog-dev
-sudo apt install build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev liblzma-dev netcat
+#!/usr/bin/env bash
+sudo apt-get install -y liblzma-dev libbz2-dev libzstd-dev libsndfile1-dev libopenblas-dev libfftw3-dev libgflags-dev libgoogle-glog-dev
+sudo apt install -y build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev liblzma-dev netcat
 
 
 git clone https://github.com/kpu/kenlm.git
@@ -7,7 +8,7 @@ cd kenlm
 mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DKENLM_MAX_ORDER=20 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 make -j16
-cd ..
+cd ../../
 export KENLM_ROOT_DIR=$PWD
 export USE_CUDA=0 ## for cpu
 
@@ -19,7 +20,6 @@ git clone https://github.com/facebookresearch/wav2letter.git -b v0.2
 cd wav2letter/bindings/python
 pip install -e .
 cd ../../../
-
 rm -rf wav2letter
 
 
