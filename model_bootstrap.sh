f2fbcf6d94142e78a62df8f02bb26abf40829b8f
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-04-21 12:22:27 +0530

    |Soujyo + niresh | Fix the model bootstrap code

--- model_bootstrap.sh
+++ model_bootstrap.sh
@@ -2,27 +2,29 @@
 
 echo "Setting up model dependencies...."
 sudo apt-get update
-sudo apt-get install -y liblzma-dev libbz2-dev libzstd-dev libsndfile1-dev libopenblas-dev libfftw3-dev libgflags-dev libgoogle-glog-dev
-sudo apt install -y build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev liblzma-dev netcat
+sudo apt-get install liblzma-dev libbz2-dev libzstd-dev libsndfile1-dev libopenblas-dev libfftw3-dev libgflags-dev libgoogle-glog-dev
+sudo apt install build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev liblzma-dev
 sudo apt-get install -y ffmpeg
 
 git clone https://github.com/kpu/kenlm.git
 cd kenlm
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DKENLM_MAX_ORDER=20 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-make -j16
+cmake ..
+make -j 16
+cd ..
 export KENLM_ROOT_DIR=$PWD
 export USE_CUDA=0 ## for cpu
-cd ../../
+cd ..
 
 # rm -rf kenlm
 
 
 
-git clone https://github.com/facebookresearch/wav2letter.git -b v0.2
-cd wav2letter/bindings/python
+git clone -b v0.2 https://github.com/facebookresearch/wav2letter.git
+cd wav2letter
+git checkout b1d1f89f586120a978a4666cffd45c55f0a2e564
+cd bindings/python
 pip install -e .
-cd ../../../
 # rm -rf wav2letter
 
 
@@ -31,8 +33,3 @@ cd wav2vec-infer/wav2vec
 pip install -e .
 cd ../../
 
-# rm -rf wav2vec-infer
-if [ "$1" = "start_server" ];then
-  echo "Starting the grpc server...."
-  python /opt/speech_recognition_open_api/server.py
-fi
