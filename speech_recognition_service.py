8a638e3555785ee7f49bbf9345851aab70c6ef54
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-04-26 12:02:10 +0530

    |Niresh + Soujyo| implementation for updated proto

--- speech_recognition_service.py
+++ speech_recognition_service.py
@@ -17,6 +17,7 @@ class SpeechRecognizer(speech_recognition_open_api_pb2_grpc.SpeechRecognizerServ
         else:
             audio_path = create_wav_file_using_bytes(request.file_name, request.audio_bytes)
         response = self.model_service.transcribe(audio_path, request.language)
+        os.remove(audio_path)
         return RecognitionOutput(result=response['transcription'])
         # if len(request.audio_url) != 0:
         #     return RecognitionOutput(result=request.audio_url)
@@ -24,6 +25,15 @@ class SpeechRecognizer(speech_recognition_open_api_pb2_grpc.SpeechRecognizerServ
         #     return RecognitionOutput(result=str(request.audio_bytes))
 
     def recognizeV2(self, request, context):
-        print("Called", request)
-        print(Language.LanguageCode.Name(request.config.language.value))
-        return SpeechRecognitionResult(status='SUCCESS')
+        language = Language.LanguageCode.Name(request.config.language.value)
+        audio_format = RecognitionConfig.AudioFormat.Name(request.config.audioFormat)
+        file_name = 'audio_input_{}.{}'.format(str(get_current_time_in_millis()), audio_format.lower())
+        if len(request.audio.audioUri) != 0:
+            audio_path = download_from_url_to_file(file_name, request.audio.audioUri)
+        elif len(request.audio.fileId) != 0:
+            return SpeechRecognitionResult(status='NO_MATCH')
+        else:
+            audio_path = create_wav_file_using_bytes(file_name, request.audio.audioContent)
+        response = self.model_service.transcribe(audio_path, language)
+        os.remove(audio_path)
+        return SpeechRecognitionResult(status='SUCCESS', transcript=response['transcription'])
--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import requests
 import wave
 import os
+import time
 
 
 def download_from_url_to_file(file_name, url):
@@ -18,10 +19,16 @@ def create_wav_file_using_bytes(file_name, audio):
         file.writeframes(audio)
     return os.path.join(os.getcwd(), file_name)
 
+
 def write_to_file(file_name, audio):
     with open(file_name, 'wb') as f:
         f.write(audio)
-    return os.path.join(os.getcwd(), file_name) 
+    return os.path.join(os.getcwd(), file_name)
+
 
 def convert_audio_to_required_format(file_name):
     pass
+
+
+def get_current_time_in_millis():
+    return round(time.time() * 1000)
