4616ce061d2c7599be7f178017124d5362565309
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-04-27 15:14:55 +0530

    |Niresh| added example for v2 java

--- examples/java/speech-recognition-client/src/main/java/com/ekstep/endpoints/speech_recognition/SpeechRecognitionClient.java
+++ examples/java/speech-recognition-client/src/main/java/com/ekstep/endpoints/speech_recognition/SpeechRecognitionClient.java
@@ -35,6 +35,29 @@ public class SpeechRecognitionClient {
         }
     }
 
+    public SpeechRecognitionResult transcribeUrlV2() {
+        String audioUrl = "https://codmento.com/ekstep/test/changed.wav";
+        logger.info("Will try to request " + audioUrl + " ...");
+        RecognitionConfig config = RecognitionConfig.newBuilder()
+                .setLanguage(Language.newBuilder().setValue(Language.LanguageCode.hi).build())
+                .setAudioFormat(RecognitionConfig.AudioFormat.WAV)
+                .build();
+        RecognitionAudio audio = RecognitionAudio.newBuilder().setAudioUri(audioUrl).build();
+        SpeechRecognitionRequest request = SpeechRecognitionRequest.newBuilder()
+                .setAudio(audio)
+                .setConfig(config)
+                .build();
+        SpeechRecognitionResult response;
+
+        try {
+            response = blockingStub.recognizeV2(request);
+            return response;
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return SpeechRecognitionResult.newBuilder().build();
+        }
+    }
+
     public RecognitionOutput transcribeBytes() {
         logger.info("Will try to request ...");
         AudioFiles afiles = new AudioFiles();
@@ -54,6 +77,33 @@ public class SpeechRecognitionClient {
         }
     }
 
+    public SpeechRecognitionResult transcribeBytesV2() {
+        logger.info("Will try to request ...");
+        AudioFiles audioFiles = new AudioFiles();
+        String file = "/Users/nireshkumarr/Documents/ekstep/speech-recognition-open-api/examples/python/speech-recognition/changed.wav";
+        byte[] data2 = audioFiles.readAudioFileData(file);
+        ByteString byteString = ByteString.copyFrom(data2);
+
+        RecognitionConfig config = RecognitionConfig.newBuilder()
+                .setLanguage(Language.newBuilder().setValue(Language.LanguageCode.hi).build())
+                .setAudioFormat(RecognitionConfig.AudioFormat.WAV)
+                .build();
+        RecognitionAudio audio = RecognitionAudio.newBuilder().setAudioContent(byteString).build();
+        SpeechRecognitionRequest request = SpeechRecognitionRequest.newBuilder()
+                .setAudio(audio)
+                .setConfig(config)
+                .build();
+
+        SpeechRecognitionResult response;
+        try {
+            response = blockingStub.recognizeV2(request);
+            return response;
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return SpeechRecognitionResult.newBuilder().build();
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
         String target = "34.70.114.226:50051";
@@ -63,9 +113,18 @@ public class SpeechRecognitionClient {
                 .build();
         try {
             SpeechRecognitionClient client = new SpeechRecognitionClient(channel);
-            RecognitionOutput response = client.transcribeBytes();
-//            RecognitionOutput response = client.transcribeUrl();
-            System.out.println(response.getResult());
+            /* v1 */
+            /* RecognitionOutput bytesResponse = client.transcribeBytes();
+            RecognitionOutput urlResponse = client.transcribeUrl();
+            System.out.println(bytesResponse.getResult());
+            System.out.println(urlResponse.getResult()); */
+
+            /* v2 */
+            SpeechRecognitionResult bytesResponse = client.transcribeBytesV2();
+            SpeechRecognitionResult urlResponse = client.transcribeUrlV2();
+            System.out.println(bytesResponse.getTranscript());
+            System.out.println(urlResponse.getTranscript());
+
         } finally {
             channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
         }
