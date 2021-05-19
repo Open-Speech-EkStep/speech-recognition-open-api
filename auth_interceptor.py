8465f7d9c2b61b67e2dfc5e2cc2d851a68fcc39d
NireshKumarRaj <nireshkumar.r@thoughtworks.com>
2021-05-19 06:30:25 +0000

    Niresh | Added auth inteerceptor

---
+++ auth_interceptor.py
@@ -0,0 +1,23 @@
+import grpc
+
+
+class AuthInterceptor(grpc.ServerInterceptor):
+    def __init__(self, key):
+        self._valid_metadata = ('authorization', key)
+
+        def deny(_, context):
+            context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Invalid key')
+
+        self._deny = grpc.unary_unary_rpc_method_handler(deny)
+
+    def intercept_service(self, continuation, handler_call_details):
+        meta = handler_call_details.invocation_metadata
+        print(handler_call_details)
+        print("\n\n")
+        print(meta)
+        print("\n\n")
+        print(self._valid_metadata)
+        if meta and meta[0] == self._valid_metadata:
+            return continuation(handler_call_details)
+        else:
+            return self._deny
--- server.py
+++ server.py
@@ -3,10 +3,14 @@ import grpc
 from speech_recognition_service import SpeechRecognizer
 from stub.speech_recognition_open_api_pb2_grpc import add_SpeechRecognizerServicer_to_server
 from inference_lib.wav2vec_ctc import Wav2VecCtc
+from auth_interceptor import AuthInterceptor
 
 
 def run():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        interceptors=(AuthInterceptor('Bearer mysecrettoken'),)
+    )
     add_SpeechRecognizerServicer_to_server(SpeechRecognizer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
