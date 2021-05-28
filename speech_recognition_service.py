3a6f840b2e824f23321366a763c49ba72a8e17ea
NireshKumarRaj <nireshkumar.r@thoughtworks.com>
2021-05-28 09:56:07 +0000

    Add exception for generic ones

--- speech_recognition_service.py
+++ speech_recognition_service.py
@@ -26,17 +26,20 @@ class SpeechRecognizer(speech_recognition_open_api_pb2_grpc.SpeechRecognizerServ
         audio_format = RecognitionConfig.AudioFormat.Name(request.config.audioFormat)
         out_format = RecognitionConfig.TranscriptionFormat.Name(request.config.transcriptionFormat)
         file_name = 'audio_input_{}.{}'.format(str(get_current_time_in_millis()), audio_format.lower())
-        if len(request.audio.audioUri) != 0:
-            audio_path = download_from_url_to_file(file_name, request.audio.audioUri)
-        elif len(request.audio.fileId) != 0:
-            return SpeechRecognitionResult(status='NO_MATCH')
-        else:
-            audio_path = create_wav_file_using_bytes(file_name, request.audio.audioContent)
-        if out_format == 'SRT':
-            response = self.model_service.get_srt(audio_path, language)
-            result = SpeechRecognitionResult(status='SUCCESS', srt=response['srt'])
-        else:
-            response = self.model_service.transcribe(audio_path, language)
-            result = SpeechRecognitionResult(status='SUCCESS', transcript=response['transcription'])
-        os.remove(audio_path)
-        return result
+        try:
+            if len(request.audio.audioUri) != 0:
+                audio_path = download_from_url_to_file(file_name, request.audio.audioUri)
+            elif len(request.audio.fileId) != 0:
+                return SpeechRecognitionResult(status='NO_MATCH')
+            else:
+                audio_path = create_wav_file_using_bytes(file_name, request.audio.audioContent)
+            if out_format == 'SRT':
+                response = self.model_service.get_srt(audio_path, language)
+                result = SpeechRecognitionResult(status='SUCCESS', srt=response['srt'])
+            else:
+                response = self.model_service.transcribe(audio_path, language)
+                result = SpeechRecognitionResult(status='SUCCESS', transcript=response['transcription'])
+            os.remove(audio_path)
+            return result
+        except Exception as e:
+            print("An exception has occured.Please verify inputs...")
