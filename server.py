ed6f9d1ace9cb77acd695b1edce38fbde82304da
ankit-thoughtworks <ankit.katiyar@thoughtworks.com>
2021-12-31 13:14:41 +0530

    |Ankit| add Wav2VecCtc import

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import grpc
 from src.speech_recognition_service import SpeechRecognizer
 from stub.speech_recognition_open_api_pb2_grpc import add_SpeechRecognizerServicer_to_server
 
+from fairseq.models.wav2vec.wav2vec2_asr import Wav2VecCtc
+
 
 def run():
     server = grpc.server(
