2efd8e97987451bc485fa2a7dd90745966546d93
soujyo <soujyo.sen@thoughtworks.com>
2022-01-13 10:29:35 +0530

    |Soujyo  | Fix dir name in denioser

--- src/srt/subtitle_generator.py
+++ src/srt/subtitle_generator.py
@@ -1,28 +1,27 @@
 import os
-import shutil
 import subprocess
 
 import torch
 
 from src import log_setup
-from src.srt.infer import generate_srt
 from src.media_convertor import media_conversion
+from src.srt.infer import generate_srt
 
 LOGGER = log_setup.get_logger(__name__)
 
 
-def noise_suppression(dir_name, denoiser_path):
+def noise_suppression(dir_name: str, denoiser_path):
     cwd = os.getcwd()
     os.chdir(denoiser_path)
     subprocess.call([
-                        "python -m denoiser.enhance --dns48 --noisy_dir {} --out_dir {} --sample_rate {} --num_workers {} --device cpu".format(
-                            dir_name, dir_name, 16000, 1)], shell=True)
+        "python -m denoiser.enhance --dns48 --noisy_dir {} --out_dir {} --sample_rate {} --num_workers {} --device cpu".format(
+            dir_name, dir_name, 16000, 1)], shell=True)
     os.chdir(cwd)
 
 
 def get_srt(file, model, generator, dict_path, denoiser_path, audio_threshold=5, language='hi', half=False):
     dir_name = media_conversion(file, duration_limit=audio_threshold)
-    noise_suppression(dir_name, denoiser_path)
+    noise_suppression(str(dir_name), denoiser_path)
     audio_file = dir_name / 'clipped_audio_enhanced.wav'
 
     result = generate_srt(wav_path=audio_file, language=language, model=model, generator=generator,
