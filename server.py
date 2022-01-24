3b0ae060963dde6fa4fd5f2b7d3808e23f356cff
ankit-thoughtworks <ankit.katiyar@thoughtworks.com>
2022-01-24 12:00:44 +0530

    |Ankit| make max_workers environment variable

--- server.py
+++ server.py
@@ -2,13 +2,16 @@ from concurrent import futures
 import grpc
 from src.speech_recognition_service import SpeechRecognizer
 from stub.speech_recognition_open_api_pb2_grpc import add_SpeechRecognizerServicer_to_server
+from src.utilities import get_env_var
 
 from src.lib.inference_lib import Wav2VecCtc
 
 
 def run():
+    workers = get_env_var('max_workers', 10)
+    print('Using server workers:', workers)
     server = grpc.server(
-        futures.ThreadPoolExecutor(max_workers=10),
+        futures.ThreadPoolExecutor(max_workers=workers),
         # interceptors=(AuthInterceptor('Bearer mysecrettoken'),)
     )
     add_SpeechRecognizerServicer_to_server(SpeechRecognizer(), server)
